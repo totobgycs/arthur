76f8e19910a246caaa34f6cd714d040e28d77ba9
Ubuntu <ubuntu@ip-172-31-34-88.eu-west-1.compute.internal>
2019-01-26 12:14:56 +0000

    add unigram support to the model build

--- english/build_model_incremental.py
+++ english/build_model_incremental.py
@@ -33,10 +33,38 @@ def setup():
   param_model = args.model
   param_corpus = args.corpus
 
-def process_followers(word1, pos, sentence):
+def process_ngram1(pos, sentence):
+  n = 1
+  word1 = sentence[pos]
+  word1 = word1.upper()
+
+  # read the unigram
+  sql_cmd = 'select word1, cooc from prominence_n1 where word1 = ?'
+  sql_cmd_bindings = (word1,)
+  cursor.execute(sql_cmd, sql_cmd_bindings)
+  rec = cursor.fetchall()
+  # compute new weights
+  if not rec:
+    c = 1 * param_alpha
+    sql_cmd = 'INSERT INTO prominence_n1(word1, cooc) VALUES(?, ?)'
+    sql_cmd_bindings=(word1, c)
+  else:
+    oldc = rec[0][n]
+    c = oldc * (1 - param_alpha) + 1 * param_alpha
+    sql_cmd = 'UPDATE prominence_n1 set cooc = ? WHERE word1 = ?'
+    sql_cmd_bindings=(c, word1)
+  # update the unigram
+  cursor.execute(sql_cmd, sql_cmd_bindings)
+  print("Updated unigram: {w1}".format(w1=word1))
+  db.commit()
+   
+
+def process_ngram2(pos, sentence):
+  n = 2
   # if this is the last word, there is nothing to do
   if pos == len(sentence) - 1:
     return
+  word1 = sentence[pos]
   word2 = sentence[pos + 1]
   word1 = word1.upper()
   word2 = word2.upper()
@@ -46,18 +74,19 @@ def process_followers(word1, pos, sentence):
   sql_cmd_bindings = (word1, word2)
   cursor.execute(sql_cmd, sql_cmd_bindings)
   rec = cursor.fetchall()
+  # compute new weights
   if not rec:
     c = 1 * param_alpha
     sql_cmd = 'INSERT INTO prominence_n2(word1, word2, cooc) VALUES(?, ?, ?)'
     sql_cmd_bindings=(word1, word2, c)
   else:
-    oldc = rec[0][2]
+    oldc = rec[0][n]
     c = oldc * (1 - param_alpha) + 1 * param_alpha
     sql_cmd = 'UPDATE prominence_n2 set cooc = ? WHERE word1 = ? and word2 = ?'
     sql_cmd_bindings=(c, word1, word2)
   # update the bigram
   cursor.execute(sql_cmd, sql_cmd_bindings)
-  print("Updated: {w1}, {w2}".format(w1=word1,w2=word2))
+  print("Updated bigram: {w1}, {w2}".format(w1=word1,w2=word2))
   db.commit()
    
 
@@ -67,8 +96,27 @@ def process_corpus():
       for sentence in nltk.sent_tokenize(line):
         tokenized_sentence = nltk.word_tokenize(sentence)
         for pos, word in enumerate(tokenized_sentence):
-          process_followers(word, pos, tokenized_sentence)
+          process_ngram1(pos, tokenized_sentence)
+          # process_ngram2(pos, tokenized_sentence)
   print("Done processing corpus")
+
+
+def print_unigram_stats():
+  n = 1
+  print("Top prominances:")
+  sql_cmd = 'select word1, cooc from prominence_n1 order by cooc desc limit 20'
+  records = cursor.execute(sql_cmd)
+  for record in records:
+    print("{w1}\t : {c}".format(w1=record[0], c=record[n]))
+
+
+def print_bigram_stats():
+  n = 2
+  print("Top co-occurrances:")
+  sql_cmd = 'select word1, word2, cooc from prominence_n2 order by cooc desc limit 20'
+  records = cursor.execute(sql_cmd)
+  for record in records:
+    print("{w1}\t-\t{w2}\t : {c}".format(w1=record[0], w2=record[1], c=record[n]))
 
 
 def main():
@@ -85,11 +133,8 @@ def main():
   
   process_corpus()
 
-  print("Top co-occurrances:")
-  sql_cmd = 'select word1, word2, cooc from prominence_n2 order by cooc desc limit 20'
-  records = cursor.execute(sql_cmd)
-  for record in records:
-    print("{w1}\t-\t{w2}\t : {c}".format(w1=record[0], w2=record[1], c=record[2]))
+  print_unigram_stats()
+  # print_bigram_stats()
 
   print("Done")
 
